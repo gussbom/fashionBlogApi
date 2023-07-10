dc6bac0ea44d2dced0f678ce54b17bc74f96423f
Decadev <Decadev@Augustines-MacBook-Air.local>
2023-07-10 21:07:57 +0100

    Task creation update.

---
+++ src/main/java/com/gusso/fashionblog_api/FashionblogApiApplication.java
@@ -0,0 +1,13 @@
+package com.gusso.fashionblog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionblogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionblogApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gusso/fashionblog_api/FashionblogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gusso.fashionblog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionblogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
